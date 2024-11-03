1fc54faa5676302f119fca5f184d62cbf56b78cb
Srique8 <e.sanchez.86468@gmail.com>
2024-11-02 20:12:11 -0700

    finalized base for local mulitplayer

--- boundaryScript.gd
+++ boundaryScript.gd
@@ -5,4 +5,5 @@ extends Area2D
 #if overlapping with the player deal damage
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		$"../CharacterBody2D".health -= 100
+		$"../Player1".health -= 100
+		$"../Player2".health -= 100
