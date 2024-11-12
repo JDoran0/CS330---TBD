9b384eaf2e23fda7d6eccff24e3bf581d3e41e3b
JerryCariaga <jerichocariaga360@gmail.com>
2024-11-12 11:18:29 -0800

    Audio + SFX + new menu layouts. Please be careful when merging this.

---
+++ menus/game_over.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_re_match_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_new_match_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_return_to_menu_pressed() -> void:
+	pass # Replace with function body.
