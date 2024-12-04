5f746abb442b7348056855e0213e923cf20fad5f
Srique8 <e.sanchez.86468@gmail.com>
2024-12-03 22:31:42 -0800

    Final chicken implementation along with art finished

--- Weapon--Chicken/concussed_effect.gd
+++ Weapon--Chicken/concussed_effect.gd
@@ -1,20 +1,21 @@
 extends CanvasLayer
 
-@onready var Chicken1 = $Sprite2D
-@onready var Chicken2 = $Sprite2D2
+
 @onready var animation = $AnimationPlayer
+@onready var Chick1 = $Sprite2D
+@onready var Chick2 = $Sprite2D2
 
 
 func _ready() -> void:
-	Chicken1.visible = false
-	Chicken2.visible = false
+	Chick1.visible = false
+	Chick2.visible = false
 	animation.animation_finished.connect(_on_animation_finished)
 	
-func _on_animation_finished(AnimPlayed):
-	Chicken1.visible = false
-	Chicken2.visible = false
+func _on_animation_finished(_AnimPlayed):
+	Chick1.visible = false
+	Chick2.visible = false
 
 func playConcussedEffect():
-	Chicken1.visible = true
-	Chicken2.visible = true
+	Chick1.visible = true
+	Chick2.visible = true
 	animation.play("Flash")
